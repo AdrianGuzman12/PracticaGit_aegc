406cee64442bb166efd2433e5362cf0d42f45c6a
jaelCM <jael.cruz_pm24321@cecyte.edu.mx>
2026-03-05 12:01:30 -0600

    fix: añadida calculadora básica

---
+++ src/main/java/com/mycompany/practicagit_aegc/PracticaGIT_aegc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicagit_aegc;
+
+/**
+ *
+ * @author Alumno
+ */
+public class PracticaGIT_aegc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
