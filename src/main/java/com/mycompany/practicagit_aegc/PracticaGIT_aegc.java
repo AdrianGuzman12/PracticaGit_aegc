be9b27f47fd362be8258123b603465c15fa933ea
jaelCM <jael.cruz_pm24321@cecyte.edu.mx>
2026-03-09 07:55:26 -0600

    fix: añadida calculadora básica

--- src/main/java/com/mycompany/practicagit_aegc/PracticaGIT_aegc.java
+++ src/main/java/com/mycompany/practicagit_aegc/PracticaGIT_aegc.java
@@ -11,14 +11,6 @@ package com.mycompany.practicagit_aegc;
 public class PracticaGIT_aegc {
 
     public static void main(String[] args) {
-        System.out.println
-    int n1, n2, suma;
-      System.out.println("");  
-    n,1
-        
-        
-        System.out.println("");
- 
-    
+        System.out.println("La suma de 2 números");
     }
 }
