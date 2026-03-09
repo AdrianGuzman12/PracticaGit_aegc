f047fd2fc486b42cf71a65ad5c4b9b7da0188a08
jaelCM <jael.cruz_pm24321@cecyte.edu.mx>
2026-03-09 07:29:53 -0600

    fix: añadida calculadora básica

--- src/main/java/com/mycompany/practicagit_aegc/PracticaGIT_aegc.java
+++ src/main/java/com/mycompany/practicagit_aegc/PracticaGIT_aegc.java
@@ -11,7 +11,7 @@ package com.mycompany.practicagit_aegc;
 public class PracticaGIT_aegc {
 
     public static void main(String[] args) {
-        System.out.println("Sistema de gestión de alumnos ");
+        System.out.println("La suma de 2 números ");
     
     
     }
